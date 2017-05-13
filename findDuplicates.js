7111facdac62f8dbd2456162b235933dd5d9069f
Leo Lee <nxl1969@gmail.com>
2017-05-13 12:33:40 -0400

    LeetCode 442. Find All Duplicates in an Array(Added solution 2)

--- findDuplicates.js
+++ findDuplicates.js
@@ -39,3 +39,17 @@ var findDuplicates = function(nums) {
     }
     return res;
 };
+
+//solution 2:  when find a number i, flip the number at position i-1 to negative. if the number at position i-1 is already negative, i is the number that occurs twice.
+var findDuplicates = function(nums) {
+	let res=[];
+    for(let i=0;i<nums.length;i++)
+    {
+      
+        let idx = Math.abs(nums[i]) -1;
+        if(nums[idx] < 0)
+            res.push(Math.abs(idx+1));
+        nums[idx] = -nums[idx];
+        
+    }
+    return res;
